02bcf912e67b4d67d3bf75ee56b542847ea6cef5
Joan Queralt <jqueraltmo@gmail.com>
2012-06-19 12:54:58 +0200

    - Included/modified license text to all files.

--- actions/advpoll/save.php
+++ actions/advpoll/save.php
@@ -1,15 +1,11 @@
 <?php
-
-/*
- * Copyright 2012 DRY Team
- *              - aruberuto
- *              - joker
- *              - *****
- *              y otros
+/**
+ * Polls plugin for elgg-1.8
+ * Copyright 2012 Lorea, DRY Team
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -22,6 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
  */
+
 elgg_load_library('advpoll:model');
 
 $title = get_input('title');
--- actions/advpoll/vote.php
+++ actions/advpoll/vote.php
@@ -1,16 +1,11 @@
 <?php
 /**
- * mod/advpoll/actions/vote.php
- * 
- * Copyright 2012 DRY Team
- *              - aruberuto
- *              - joker
- *              - *****
- *              y otros
+ * Polls plugin for elgg-1.8
+ * Copyright 2012 Lorea, DRY Team
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
